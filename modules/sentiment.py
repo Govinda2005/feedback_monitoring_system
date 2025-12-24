5fd79b4239785a8ad5037eb6ba8f8a16ac56b686
GUDISA GOVINDA <govindagudisa3427@gmail.com>
2025-12-24 20:13:02 +0530

    Add positive and negative keywords for sentiment analysis

---
+++ modules/sentiment.py
@@ -0,0 +1,10 @@
+POSITIVE_KEYWORDS = {
+    "good", "great", "excellent", "fast", "happy",
+    "satisfied", "awesome", "thanks", "thank you"
+}
+
+NEGATIVE_KEYWORDS = {
+    "bad", "poor", "delay", "late", "crash",
+    "crashing", "issue", "problem", "worst",
+    "billing", "charged", "slow"
+}
