fe191a50a4461c8049e1f71f8586b9345e88a697
GUDISA GOVINDA <govindagudisa3427@gmail.com>
2025-12-24 20:03:48 +0530

    Integrate feedback reading into main execution

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from modules.reader import read_feedback_today
+
 if __name__ == "__main__":
-    print("Feedback Monitoring System started...")
+    print("System started...")
+
+    feedback = read_feedback_today()
+
+    print("Feedback read from file:")
+    for item in feedback:
+        print(item)
